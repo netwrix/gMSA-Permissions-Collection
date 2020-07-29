e18020a592eef97a87422a0a48791f7414c0ac26
Kevin Joyce <44587322+kdejoyce@users.noreply.github.com>
2020-07-29 17:14:56 -0400

    Update gMSA_Permissions_Collection.ps1

--- gMSA_Permissions_Collection.ps1
+++ gMSA_Permissions_Collection.ps1
@@ -1,4 +1,10 @@
-
+<#
+Author: Kevin Joyce
+Requirements: Active Directory PowerShell module, Domain Administrator privileges (to ensure the capability to get attribute GUIDs and view all permissions on all gMSA objects)
+Description: Looks up permissions within Active Directory on a gMSA to determine access to modify the gMSA attribute (ms-ds-GroupMSAMembership).
+Usage: opuplate the $target varbiable with the samaccountname of a gMSA.
+To output the results to a text file run the following .\LAPS_Permissions_Collection.ps1 > output.txt
+#>
 Import-Module ActiveDirectory
 ##Get the GUID of the extended attribute ms-ds-GroupMSAMembership from Schema
 $schemaIDGUID = @{}
